7a7c9ebfb055ecff1fddd11b57c829c1979ed304
qudcks8084 <qudcks8084@naver.com>
2024-04-20 19:32:23 +0900

    20240421

--- Baekjoon/src/Main1181.java
+++ Baekjoon/src/Main1181.java
@@ -8,32 +8,46 @@ public class Main1181 {
         Scanner sc = new Scanner(System.in);
         int numOfWords = Integer.parseInt(sc.nextLine());
         String[] array = new String[numOfWords];
-        String temp ;
         for(int i = 0 ; i < numOfWords ; i++){
             array[i] = sc.nextLine();
         }
         HashSet<String> hashSet = new HashSet<>(Arrays.asList(array));
         String[] resultArr = hashSet.toArray(new String[0]);
 
-        boolean swapped = false;
-        for (int i = 0; i < resultArr.length - 1; i++) {
-            for (int j = 0; j < resultArr.length - 1 ; j++) {
-                if (compareFunction(resultArr[j],resultArr[j+1]) == 1) {
-                    temp = resultArr[j];
-                    resultArr[j] = resultArr[j+1];
-                    resultArr[j+1] = temp;
-                    swapped = true;
-                }
-            }
-            if (!swapped) break;
-        }
-
-
+        quickSort(resultArr, 0, resultArr.length -1);
 
         for (String result : resultArr) {
             System.out.println(result);
         }
+    }
 
+    static void quickSort(String[] data, int left, int right) {
+        int pl = left;
+        int pr = right;
+        String pivot = data[(pl + pr) / 2];
+
+        do {
+            while (compareFunction(data[pl],pivot) == 0) {
+                pl++;
+            }
+            while (compareFunction(data[pr],pivot) == 1) {
+                pr--;
+            }
+            if (pl <= pr) { // pl보다 pr이 크면 교환(오름차순)
+                swap(data, pl++, pr--);
+            }
+        } while (pl <= pr);
+
+        // 정렬 끝난 후 나누어진 두개의 그룹에 데이터 수를 체크
+        if (left < pr)
+            quickSort(data, left, pr); // left가 pr보다 작으면 그룹의 수가 1개 이상이기 때문에 다시 정렬
+        if (pl < right)
+            quickSort(data, pl, right); // pl이 right보다 작으면 그룹의 수가 1개 이상이기 때문에 다시 정렬
+    }
+    public static void swap(String[] array, int pl, int pr){
+        String temp = array[pl];
+        array[pl] = array[pr];
+        array[pr] = temp;
     }
     public static int compareFunction(String a, String b){
         if(a.length() == b.length()){
