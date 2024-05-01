286c25e7cf07529266d1a093084f1c23a35dc93d
qudcks8084 <qudcks8084@naver.com>
2024-05-02 02:23:46 +0900

    20240502

---
+++ Baekjoon/src/Main2869.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main2869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int up = sc.nextInt();
+        int down = sc.nextInt();
+        int length = sc.nextInt();
+
+        int day = (length-down)/(up-down);
+
+        if((length-down)%(up-down) != 0)
+            day++;
+
+        System.out.println(day);
+    }
+}
