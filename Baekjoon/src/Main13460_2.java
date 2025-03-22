97c5ad92a07af5039059df830ee9389ce46e1f5e
qudcks8084 <qudcks8084@naver.com>
2025-03-22 23:15:08 +0900

    2025-03-22

--- Baekjoon/src/Main13460_2.java
+++ Baekjoon/src/Main13460_2.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.StringTokenizer;
@@ -7,9 +8,10 @@ import java.util.StringTokenizer;
 public class Main13460_2 {
 
     static int H, W;
-    static int min;
     static int[] O;
+    static final int UNIT = 10;
     static boolean[][] map;
+    static boolean[][] visited;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -48,22 +50,113 @@ public class Main13460_2 {
         }
 
 
+        // 최종 목표 -> 빨간 구슬을 구멍에 넣는 것
+        // 경우의 수를 관리할 visited 배열을 생성
+        // 각 구슬의 좌표는 0~9로 이루어져 있음으로 2차원 배열로 인덱싱해서 풀기
+        visited = new boolean[UNIT * UNIT][UNIT * UNIT];
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{B[0], B[1], R[0], R[1]});
+        visited[B[0] * UNIT + B[1]][R[0] * UNIT + R[1]] = true;
 
-        System.out.println("B " + Arrays.toString(B));
-        System.out.println("R " + Arrays.toString(R));
-        System.out.println("O " + Arrays.toString(O));
+        int time = 0;
+        while (!q.isEmpty()) {
+            if(time == 11) break;
+            int len = q.size();
+//            System.out.println("시간 : " + time);
+            for(int l = 0 ; l < len ; l++){
+                int[] cur = q.poll();
+//                System.out.println("기존 : " + Arrays.toString(cur));
+
+                // 정답 처리 코드 자리
+                if(cur[2] == O[0] && cur[3] == O[1]){ // 빨간공이 구멍에 들어간 경우
+                    System.out.println(time);
+                    return;
+                }
+                for(int i = 0 ; i < 4 ; i++){
+                    int[] next_B = new int[2];
+                    int[] next_R = new int[2];
+                    if(i == 0){ // 위로 가는거 -> c가 작은거부터
+                        if(cur[0] < cur[2]){
+                            next_B = move(cur[0], cur[1], i, cur[2], cur[3]);
+                            next_R = move(cur[2], cur[3], i, next_B[0], next_B[1]);
+                        }else{
+                            next_R = move(cur[2], cur[3], i, cur[0], cur[1]);
+                            next_B = move(cur[0], cur[1], i, next_R[0], next_R[1]);
+                        }
+                    }
+                    else if (i == 1) { // 오른쪽으로 가는거 -> r이 큰거부터
+                        if(cur[1] < cur[3]){
+                            next_R = move(cur[2], cur[3], i, cur[0], cur[1]);
+                            next_B = move(cur[0], cur[1], i, next_R[0], next_R[1]);
+                        }else{
+                            next_B = move(cur[0], cur[1], i, cur[2], cur[3]);
+                            next_R = move(cur[2], cur[3], i, next_B[0], next_B[1]);
+                        }
+                    }
+                    else if (i == 2) {
+                        if(cur[0] > cur[2]){ // 아래로 가는거 -> c가 큰거부터
+                            next_B = move(cur[0], cur[1], i, cur[2], cur[3]);
+                            next_R = move(cur[2], cur[3], i, next_B[0], next_B[1]);
+                        }else{
+                            next_R = move(cur[2], cur[3], i, cur[0], cur[1]);
+                            next_B = move(cur[0], cur[1], i, next_R[0], next_R[1]);
+                        }
+                    }
+                    else {
+                        if(cur[1] > cur[3]){ // 왼쪽으로 가는거 -> r이 작은거부터
+                            next_R = move(cur[2], cur[3], i, cur[0], cur[1]);
+                            next_B = move(cur[0], cur[1], i, next_R[0], next_R[1]);
+                        }else{
+                            next_B = move(cur[0], cur[1], i, cur[2], cur[3]);
+                            next_R = move(cur[2], cur[3], i, next_B[0], next_B[1]);
+                        }
+                    }
+
+                    // 파란 공이 구멍에 들어간 경우는 제외
+                    if(next_B[0] == O[0] && next_B[1] == O[1]) continue;
+
+                    // 중복된 방문을 제거
+                    if(visited[next_B[0] * UNIT + next_B[1]][next_R[0] * UNIT + next_R[1]]) continue;
+                    visited[next_B[0] * UNIT + next_B[1]][next_R[0] * UNIT + next_R[1]] = true;
+                    q.offer(new int[]{next_B[0], next_B[1], next_R[0], next_R[1]});
+//                    print(i, next_B, next_R);
+                }
+
+            }
+            time++;
+        }
+
+        System.out.println(-1);
+
     }
 
     static int[] dc = {-1, 0, 1, 0};
     static int[] dr = {0, 1, 0, -1};
-    public static boolean move(int[] Ball, int d){
+    public static int[] move(int c, int r, int d, int noc, int nor){
         ArrayDeque<int[]> q = new ArrayDeque<>();
-        return true;
+        q.offer(new int[]{c, r});
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int n_c = cur[0] + dc[d];
+            int n_r = cur[1] + dr[d];
+            if(n_c < 0 || n_c >= H || n_r < 0 || n_r >= W) return cur; // 경계값
+            if(n_c == O[0] && n_r == O[1]) return new int[]{n_c, n_r}; // 구멍에 들어가는 경우
+            if(n_c == noc && n_r == nor) return cur;
+            if(map[n_c][n_r]) return cur;
+            q.offer(new int[]{n_c, n_r});
+        }
+
+        return new int[]{c, r};
     }
 
 
-    public static void print(int[] B, int[] R){
+    public static void print(int d, int[] B, int[] R){
         StringBuilder sb = new StringBuilder();
+        sb.append("방향 : " + d).append("\n");
+        sb.append("R ").append(Arrays.toString(R)).append(" | ");
+        sb.append("B ").append(Arrays.toString(B)).append(" | ");
+        sb.append("O ").append(Arrays.toString(O)).append("\n");
         for (int c = 0; c < H; c++) {
             for(int r = 0 ; r < W ; r++){
                 if(B[0] == c && B[1] == r) sb.append("B ");
---
+++ Baekjoon/src/Main17136.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main17136 {
+
+    static int N = 10;
+    static int minPaper;
+    static int M;
+    static ArrayList<int[]> blank;
+    static int[] dn = {5, 4, 3, 2, 1};
+    static int[] stock = {5, 5, 5, 5, 5};
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean[][] map = new boolean[N][N];
+
+        blank = new ArrayList<>();
+
+        for (int c = 0; c < N; c++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int r = 0 ; r < N ; r++){
+                boolean flag = st.nextToken().equals("1");
+                if(flag){
+                    blank.add(new int[]{c, r});
+                    map[c][r] = true;
+                }
+            }
+        }
+
+        if(blank.isEmpty()) {
+            System.out.println(0);
+            return;
+        }
+
+        M = blank.size();
+        minPaper = Integer.MAX_VALUE/10;
+        dfs(0, 0, map);
+
+        if(minPaper == Integer.MAX_VALUE/10) System.out.println(-1);
+        else System.out.println(minPaper);
+
+    }
+
+    static void dfs(int depth, int numOfPaper, boolean[][] map){
+
+        // 이미 최소값보다 크다면 종료 -> Pruning
+        if(numOfPaper >= minPaper) return;
+
+        if(depth == M){
+            minPaper = Math.min(minPaper, numOfPaper);
+            return;
+        }
+
+        int[] cur = blank.get(depth);
+        int sc = cur[0];
+        int sr = cur[1];
+
+        if(!map[sc][sr]){ // 이미 색종이로 채워진 경우
+            dfs(depth + 1, numOfPaper, map);
+            return;
+        }
+
+        for(int i = 0 ; i < 5 ; i++){
+            if(stock[i] == 0) continue;
+            int d = dn[i];
+            if (check(sc, sr, d, map)) {
+                stock[i]--;
+                boolean[][] next = new boolean[N][N];
+                for(int c = 0 ; c < N ; c++){
+                    next[c] = Arrays.copyOf(map[c], N);
+                }
+
+                for(int c = sc ; c < sc+d ; c++){
+                    for (int r = sr; r < sr + d; r++) {
+                        next[c][r] = false;
+                    }
+                }
+
+                dfs(depth + 1, numOfPaper + 1, next);
+                stock[i]++;
+            }
+
+        }
+    }
+
+    static boolean check(int sc, int sr, int d, boolean[][] map){
+        for(int c = sc ; c < sc+d ; c++){
+            for (int r = sr; r < sr + d; r++) {
+                if(c < 0 || c >= N || r < 0 || r >= N) return false;
+                if(!map[c][r]) return false;
+            }
+        }
+        return true;
+    }
+}
